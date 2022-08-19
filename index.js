c7be1d45403049b39d95f9542f18016e1d99e2f9
Alex_Bond <76467364+student-Alex-Bond@users.noreply.github.com>
2022-08-19 20:05:56 +0600

    added section cooperation

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 	const viewCookie = document.querySelector('.cookie')
 	const button = document.querySelector('.button')
 
-	setTimeout(function () {
-		viewCookie.classList.add('animationCookieUp')
-	}, 3000)
+	// setTimeout(function () {
+	// 	viewCookie.classList.add('animationCookieUp')
+	// }, 3000)
 
 	button.addEventListener('click', function () {
 		viewCookie.classList.remove('animationCookieUp')
